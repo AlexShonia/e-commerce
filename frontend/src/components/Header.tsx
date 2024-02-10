288611602d6d16ea954c1619457473ecad8f9312
AlexShonia <alex.shonia123@gmail.com>
2024-02-10 14:26:12 +0400

    inital commit, Basic Frontend

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+function Header() {
+  return (
+    <header>
+      <Navbar
+        expand="lg"
+        className="bg-body-tertiary"
+        data-bs-theme="dark"
+        collapseOnSelect
+      >
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              {" "}
+              {/* classname was me-auto before*/}
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i>Login
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
