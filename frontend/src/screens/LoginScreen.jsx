671de80e7eee73dc2e173552816d7bc16221b0eb
AlexShonia <alex.shonia123@gmail.com>
2024-03-23 16:19:01 +0400

    implement frontend user login

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -7,11 +7,24 @@ import { axiosClient } from "../axiosConfig";
 import { setLoginData } from "../features/userLoginSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import FormContainer from "../components/FormContainer";
+import {
+  useSearchParams,
+  Link,
+  redirect,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
 
 function LoginScreen() {
-  const [email, setEmail] = useState("alex");
-  const [password, setPassword] = useState("alex");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  const redirect = searchParams.has("redirect")
+    ? searchParams.get("redirect")
+    : "/";
+  const navigate = useNavigate();
   const config = {
     headers: {
       "Content-type": "application/json",
@@ -33,29 +46,56 @@ function LoginScreen() {
     {
       onSuccess: (data) => {
         console.log("Login successful", data);
-        dispatch(setLoginData({ username: email, password: password }));
+        dispatch(setLoginData(data));
+        navigate(redirect);
       },
       onError: (error) => {
-        console.log("login error", error);
+        console.log("Login error", error.response.data.detail);
       },
     }
   );
 
-  function handleSubmit() {
+  function submitHandler(e) {
+    e.preventDefault();
     mutation.mutate();
   }
+  const { error } = mutation;
 
   return (
-    <div>
-      <h1>Login</h1>
-      {mutation.isLoading ? (
-        <Loader />
-      ) : mutation.isError ? (
-        <Message variant="danger">error</Message>
-      ) : (
-        <Button onClick={handleSubmit}>Submit</Button>
-      )}
-    </div>
+    <FormContainer>
+      <h1>Sign In</h1>
+      {error && <Message variant="danger">{error.response.data.detail}</Message>}
+      {mutation.isLoading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId="password">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type="submit">Sign In</Button>
+      </Form>
+      <Row className="py-3">
+        <Col>
+          New Customer?{" "}
+          <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
   );
 }
 
