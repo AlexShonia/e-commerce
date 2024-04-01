195d01ab1a0938ec714deb0a2c693a4661403551
AlexShonia <alex.shonia123@gmail.com>
2024-04-01 15:56:24 +0400

    url fix 2

--- backend/frontend/src/axiosConfig.js
+++ backend/frontend/src/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "e-commerce-production-0fe3.up.railway.app/",
+  baseURL: "https://e-commerce-production-0fe3.up.railway.app/",
 });
